ce9f0514488fa30a56d88076b18e3be7a630a321
Iuri Campos <iribeirocampos@gmail.com>
2020-06-17 18:26:09 +0100

    Outlook parser Working

---
+++ Outlook_parser.py
@@ -0,0 +1,24 @@
+import win32com.client
+import unicodecsv as csv
+
+output_file = open("Enviados.csv", "wb")
+output_writer = csv.writer(output_file, delimiter=";", encoding="latin2")
+
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+outbox = outlook.GetDefaultFolder(5)
+# "6" refers to the index of a folder - in this case,the inbox.
+# "5" refers to the index of a folder - in this case,the outbox.
+
+messages = outbox.Items
+
+for message in messages:
+    sender = message.SenderName
+    # sender_address = message.sender.address
+    # sent_to = message.To
+    date = str(message.LastModificationTime)
+    # subject = message.subject
+    print(date)
+    output_writer.writerow([date, sender])
+    print(sender)
+
+output_file.close()
